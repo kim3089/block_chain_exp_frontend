404de201131042381e0bad0a19535e5711335823
yelshall <youssufshall@gmail.com>
2022-02-28 23:30:08 -0500

    Added message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Blockchain Explorer
         </a>
       </header>
     </div>
